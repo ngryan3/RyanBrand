1dfed3293aa47cd61b6af87126171f6a974efdd0
Sabrina Yu <sabrinayu7@gmail.com>
2020-02-26 09:27:08 -0500

    added scrolling to catalogue page

--- ryanbrand/src/react-component/Catalogue/index.js
+++ ryanbrand/src/react-component/Catalogue/index.js
@@ -4,6 +4,7 @@ import './styles.css';
 class Catalogue extends React.Component{
     render (){
         return (
+            <body>
             <div className="tileContainer">
                 <div className="tile">
                     <p>Product 1</p>
@@ -11,7 +12,50 @@ class Catalogue extends React.Component{
                 <div className="tile">
                     <p>Product 2</p>
                 </div>
+                <div className="tile">
+                    <p>Product 3</p>
+                </div>
+                <div className="tile">
+                    <p>Product 4</p>
+                </div>
+                <div className="tile">
+                    <p>Product 5</p>
+                </div>
+                <div className="tile">
+                    <p>Product 6</p>
+                </div>
+                <div className="tile">
+                    <p>Product 7</p>
+                </div>
+                <div className="tile">
+                    <p>Product 8</p>
+                </div>
+                <div className="tile">
+                    <p>Product 5</p>
+                </div>
+                <div className="tile">
+                    <p>Product 6</p>
+                </div>
+                <div className="tile">
+                    <p>Product 7</p>
+                </div>
+                <div className="tile">
+                    <p>Product 8</p>
+                </div>
+                <div className="tile">
+                    <p>Product 5</p>
+                </div>
+                <div className="tile">
+                    <p>Product 6</p>
+                </div>
+                <div className="tile">
+                    <p>Product 7</p>
+                </div>
+                <div className="tile">
+                    <p>Product 8</p>
+                </div>
             </div>
+            </body>
         )
     }
 }
