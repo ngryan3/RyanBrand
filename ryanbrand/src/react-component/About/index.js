0b8854252503fae7b7de7fb42530a374a31dc138
Kevin Yang <kevy.yang@mail.utoronto.ca>
2020-03-03 17:05:48 -0500

    added some logos and dummy links to about page

--- ryanbrand/src/react-component/About/index.js
+++ ryanbrand/src/react-component/About/index.js
@@ -3,6 +3,9 @@ import NavigationBar from '../NavigationBar'
 import Title from "../AdminDashboard/Title"
 import './index.css'
 import ryan from "../../images/ryan.png"
+import twitter from "../../images/twitter.png"
+import twitch from "../../images/twitch.png"
+import facebook from "../../images/facebook.png"
 
 class About extends React.Component {   
 
@@ -17,9 +20,11 @@ class About extends React.Component {
                 <div class="paraAbout">
 
                 <div class="paraText">{para}</div>
-
+                <a href="https://twitter.com"><img class="logoT" src={twitter}></img></a> 
+                <a href="https://facebook.com"><img class="logoF" src={facebook}></img></a> 
+                <a href="https://twitch.tv"><img class="logoCh" src={twitch}></img></a> 
                 </div>
-                <img class="ryan" src={ryan} alt="fuck"></img>
+                <img class="ryan" src={ryan} alt="a picture of ryan"></img>
                 <br></br>
             </div>
 
