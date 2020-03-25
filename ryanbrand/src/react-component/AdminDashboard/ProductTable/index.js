de82659f16625b562d0a01df86cfb055ba5b2787
Sabrina Yu <sabrinayu7@gmail.com>
2020-03-25 19:59:22 -0400

    connected product from in admin dashboard with getting products in back end

--- ryanbrand/src/react-component/AdminDashboard/ProductTable/index.js
+++ ryanbrand/src/react-component/AdminDashboard/ProductTable/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import '../UserTable/index.css';
 import Product from "../Product";
 import { uid } from "react-uid";
-
+import { getAllProducts } from "./../../../actions/product"
+const log = console.log
 
 class ProductTable extends React.Component {
-
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isLoaded: false
+        }
+    }
+    componentDidMount() {
+        getAllProducts(this)
+        log(this.state)
+    }
 
     render() {
-        const {products, component} = this.props;
+        let products = []
+        if (this.state.isLoaded === true){
+            products = this.state.data
+        }
+        const {component} = this.props;
         return(
         <div>
             <tr>
