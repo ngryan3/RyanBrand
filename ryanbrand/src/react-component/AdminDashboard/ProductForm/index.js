18ff9b38a7acd841727458486666c1c9e502a8c3
Kevin Yang <kevy.yang@mail.utoronto.ca>
2020-04-04 21:24:05 -0400

    trying to avoid conflicts

--- ryanbrand/src/react-component/AdminDashboard/ProductForm/index.js
+++ ryanbrand/src/react-component/AdminDashboard/ProductForm/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { addProduct } from "../../../actions/product";
-
 class ProductForm extends React.Component {
     state = {
         name: "",
@@ -21,14 +20,33 @@ class ProductForm extends React.Component {
 
     render() {
         return(
+            <React.Fragment>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    addProduct(this, e.target);
+                }}>
+                    <input type="text" name="name" placeholder="Name" onChange={this.handleInputChange}/>
+                    <input type="text" name="price" placeholder="Price" onChange={this.handleInputChange}/>
+                    <input type="text" name="description" placeholder="Description"  onChange={this.handleInputChange}/>
+                    <input type="text" name="category" placeholder="Category" onChange={this.handleInputChange}/>
+                    <div>
+                        <label>Image:</label>
+                        <input name="image" type="file" />
+                    </div>
+                    <button type="submit">Upload</button>
+                </form>
+            </React.Fragment>
+            /*
         <div>
             <input type="text" name="name" placeholder="Name" onChange={this.handleInputChange}/>
             <input type="text" name="price" placeholder="Price" onChange={this.handleInputChange}/>
             <input type="text" name="description" placeholder="Description"  onChange={this.handleInputChange}/>
             <input type="text" name="category" placeholder="Category" onChange={this.handleInputChange}/>
+            <ImageForm/>
             <button onClick={() => addProduct(this)}>Add product</button>
-            
+
         </div>
+             */
         )
     }
 }
--- ryanbrand/src/react-component/AdminDashboard/User/User.js
+++ ryanbrand/src/react-component/AdminDashboard/User/User.js
@@ -1,40 +1,36 @@
 import React from 'react';
+import { removeUser } from "./../../../actions/user";
 
 class User extends React.Component {
     
-    fun = () => {
-        alert("jello") 
-        //e.preventDefault();
+    // fun = () => {
+    //     alert("jello") 
+    //     //e.preventDefault();
 
-    }
+    // }
 
-    remover = (component, user) => {
-        const filtere = component.state.users.filter(u => {
-            return u !== user
-        })
+    // remover = (component, user) => {
+    //     const filtere = component.state.users.filter(u => {
+    //         return u !== user
+    //     })
 
-        component.setState({
-            users: filtere
-        })
-    }
+    //     component.setState({
+    //         users: filtere
+    //     })
+    // }
     
     render() {
         const {user, component} = this.props;
+        console.log(user)
 
         return(
         <div class="roew">
         <tr >
             <td class="first">
-                {user.name}
-            </td>
-            <td class="email">
-                {user.email}
-            </td>
-            <td class="second">
-                {user.joined}
+                {user.username}
             </td>
             <td class="third">
-                <button onClick={this.remover.bind(this, component, user)}>remove</button>
+                <button onClick={() => removeUser(component, user) }>remove</button>
             </td>            
         </tr>
         </div>
