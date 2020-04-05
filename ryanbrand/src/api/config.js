41d1dcd6f40b28e94f8d0093fb38aa99dccb0543
Kevin Yang <kevy.yang@mail.utoronto.ca>
2020-04-04 21:51:57 -0400

    getting ready for deploy

--- ryanbrand/src/api/config.js
+++ ryanbrand/src/api/config.js
@@ -1,2 +1,2 @@
-//export default "https://quiet-journey-89938.herokuapp.com"
-export default "http://localhost:5000"
+export default "https://quiet-journey-89938.herokuapp.com"
+//export default "http://localhost:5000"
