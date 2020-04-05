5f66f601cd2040d231b09e7a0b30b0102360d517
Sabrina Yu <sabrinayu7@gmail.com>
2020-04-04 21:34:28 -0400

    can add user from admin dash

--- ryanbrand/src/react-component/AdminDashboard/UserForm/index.js
+++ ryanbrand/src/react-component/AdminDashboard/UserForm/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { addUser } from '../../../actions/user';
 class UserForm extends React.Component {
+    state = {
+        username: "",
+        password: ""
+    };
 
+    handleInputChange = event => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-    
+        this.setState({
+            [name]: value
+        });
+    };
+
     render() {
-        const {
-            userName,
-            userEmail,
-            handleChange,
-            addUser
-        } = this.props;
-
         return(
         <div>
-            <input type="text" name="userName" placeholder="Name" value={userName} onChange={handleChange}></input>
-            <input type="text" name="userEmail" placeholder="Email" value={userEmail} onChange={handleChange}></input>
-            <button onClick={addUser}>Add user</button>
-            
+            <input type="text" name="username" placeholder="Username" onChange={this.handleInputChange}></input>
+            <input type="text" name="password" placeholder="Password" onChange={this.handleInputChange}></input>
+            <button onClick={() => addUser(this)}>Add user</button>
         </div>
         )
     }
